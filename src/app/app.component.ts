4b742dc79898a9aee78f59bdeebde4b3d1ec0e1d
BryanDietrichBernhardt <bryandbernhardt@gmail.com>
2021-09-29 23:13:34 -0300

    first basic version done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Todo } from './../models/todo.model';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  title: string = 'ToDo';
+  lastId: number = 1;
+  public todos: Todo[] = [];
+
+  constructor() {}
+
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo); //Captura o indice do item recebido ou retorna -1 caso não tenha
+    if (index !== -1) { //caso tenha encontrado
+      this.todos.splice(index, 1); //remove ele da lista com splice(indice, deletar um registro)
+    }
+  }
+
+  markAsDone(todo: Todo) {
+    todo.done = true;
+  }
+
+  markAsUndone(todo: Todo) {
+    todo.done = false;
+  }
+
+  addTask(input: any) {
+    this.todos.push(new Todo(this.lastId, input.value, false));
+    this.lastId++
+    input.value = ''
+  }
+
+  resetAll() {
+    this.todos = [];
+    this.lastId = 1;
+  }
 }
+
+/*
+(action)="function()"           HTML -> TS
+{{variable}}                    TS -> HTML
+[class.new-class]="condicion"   TS -> HTML
+[(ngModel)]="propriety"         TS <-> HTML
+*/
